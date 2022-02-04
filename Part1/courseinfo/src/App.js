d782b212569edee991968f31d4de126f23435ed4
Saahil Sabnis <saahilsabnis@gmail.com>
2022-02-04 23:46:02 +0530

    Updated with Exercise 1.5, consolidating all variables into one.

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -8,50 +8,48 @@ const Header = (props) => {
   )
 } 
 
-const Part = (props) => {
-  return (
-    <>
-      <p> {props.name} {props.exercise} </p>
-    </>
-  )
-}
-
 const Content = (props) => {
   return (
-    <Part name = {props.part} exercise = {props.exercise}/>
+    <>
+      <p>{props.parts[0].name} {props.parts[0].exercises} </p>
+      <p>{props.parts[1].name} {props.parts[1].exercises} </p>
+      <p>{props.parts[2].name} {props.parts[2].exercises} </p>
+    </>
   )
 }
 
 const Total = (props) => {
   return (
     <>
-      <p> Number of exercises {props.total} </p>
+      <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
     </>
   )
 }
 
 const App = () => { 
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
   
   return (
     <div>
-      <Header course = {course}/>
-      <Content part = {part1.name} exercise = {part1.exercises}/>
-      <Content part = {part2.name} exercise = {part2.exercises}/>
-      <Content part = {part3.name} exercise = {part3.exercises}/>
-      <Total total = {part1.exercises + part2.exercises + part3.exercises}/>
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
